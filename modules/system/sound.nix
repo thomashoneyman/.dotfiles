393950800d6eba3f8beb7eb01bcf815b564ee9bf
Thomas Honeyman <hello@thomashoneyman.com>
2023-07-20 11:58:28 -0400

    audio tweaks

--- modules/system/sound.nix
+++ modules/system/sound.nix
@@ -3,20 +3,28 @@
   pkgs,
   ...
 }: {
-  # Don't use pulseaudio by default
-  # https://nixos.wiki/wiki/PipeWire
-  sound.enable = false;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
 
   services.pipewire = {
     enable = true;
-
+    audio.enable = true;
+    pulse.enable = true;
     alsa = {
       enable = true;
       support32Bit = true;
     };
+    jack.enable = false;
+    wireplumber.enable = true;
+  };
 
-    pulse.enable = true;
+  services.power-profiles-daemon.enable = false;
+
+  environment.etc = {
+    "wireplumber/main.lua.d/90-suspend-timeout.lua".text = ''
+      apply_properties = {
+        ["session.suspend-timeout-seconds"] = 0;
+      };
+    '';
   };
 }
